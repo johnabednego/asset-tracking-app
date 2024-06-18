163eae25acbd3861152465b1334ac0fe7d88b2ac
johnabednego <jilimajohnabednego@gmail.com>
2024-06-18 19:45:34 +0000

    done

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const express = require('express');
-const connectDB = require('./config/db');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
 // Connect to the database
 connectDB();
 
-// Middleware
-app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 // Define routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/assets', require('./routes/assetRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 
+// Middleware to parse JSON bodies
+app.use(express.json());
 
-// Start the server
+// Swagger UI setup
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Your existing routes and middleware...
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
