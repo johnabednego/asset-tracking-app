00ab5c974b089f69b12f2400f3e3678be20696ca
johnabednego <jilimajohnabednego@gmail.com>
2024-06-18 19:36:16 +0000

    done

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/assets', require('./routes/assetRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 
+
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
