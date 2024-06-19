5ce19773ea320e52f9accc858b32875bbb88df1c
johnabednego <jilimajohnabednego@gmail.com>
2024-06-19 00:05:13 +0000

    done

--- index.js
+++ index.js
@@ -8,14 +8,8 @@ dotenv.config();
 
 const app = express();
 
-const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:5000'], // Add your frontend URLs here
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-};
-
-// Enable CORS with options
-app.use(cors(corsOptions));
+// Enable CORS
+app.use(cors());
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -30,3 +24,38 @@ app.use('/api/users', require('./routes/userRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+// const express = require('express');
+// const cors = require('cors');
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerSpec = require('./swagger');
+// const dotenv = require('dotenv');
+
+// dotenv.config();
+
+// const app = express();
+
+// const corsOptions = {
+//   origin: ['http://localhost:3000', 'http://localhost:5000'], // Add your frontend URLs here
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   credentials: true,
+// };
+
+// // Enable CORS with options
+// app.use(cors(corsOptions));
+
+// // Middleware to parse JSON bodies
+// app.use(express.json());
+
+// // Swagger UI setup
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// // Define routes
+// app.use('/api/auth', require('./routes/authRoutes'));
+// app.use('/api/assets', require('./routes/assetRoutes'));
+// app.use('/api/users', require('./routes/userRoutes'));
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
