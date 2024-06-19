7d39c94a5f7c0a9134afae4e314ff149e0838d62
johnabednego <jilimajohnabednego@gmail.com>
2024-06-19 01:00:07 +0000

    done

--- index.js
+++ index.js
@@ -1,6 +1,63 @@
-const serverless = require('serverless-http');
-const app = require('./api/index');
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+connectDB();
+
+// Enable CORS
+app.use(cors());
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Swagger UI setup
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Define routes
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/assets', require('./routes/assetRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-module.exports = app;
-module.exports.handler = serverless(app);
+
+
+
+// const express = require('express');
+// const cors = require('cors');
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerSpec = require('./swagger');
+// const dotenv = require('dotenv');
+
+// dotenv.config();
+
+// const app = express();
+
+// const corsOptions = {
+//   origin: ['http://localhost:3000', 'http://localhost:5000'], // Add your frontend URLs here
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   credentials: true,
+// };
+
+// // Enable CORS with options
+// app.use(cors(corsOptions));
+
+// // Middleware to parse JSON bodies
+// app.use(express.json());
+
+// // Swagger UI setup
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// // Define routes
+// app.use('/api/auth', require('./routes/authRoutes'));
+// app.use('/api/assets', require('./routes/assetRoutes'));
+// app.use('/api/users', require('./routes/userRoutes'));
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
